4591e14be440d951d1358f4df5a11380798269f2
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-02-12 13:17:38 -0600

    finished game

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
+let canvas;
+let canvasContext;
 
 window.onload = function () {
     canvas = document.getElementById('gameCanvas');
+    border = document.getElementById('canvas-border')
     canvasContext = canvas.getContext('2d');
-    ballX = 50;
-
-    var framesPerSecond = 30;
-    setInterval(function () {
-        moveEverything();
-        drawEverything();
-    }, 1000 / framesPerSecond);
-}
-
-function moveEverything() {
-    ballX = ballX + 5;
-}
-
-function drawEverything() {
-
+    // Game Canvas
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-    canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(0, 210, 10, 200)
+    //Other box 
     canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(ballX, 100, 10, 10);
+    canvasContext.fillRect(100, 200, 50, 25);
+
 }
+
