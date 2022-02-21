b01f2f7b868496733a9873eb607e5b18dd2c34b3
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-02-21 14:17:13 -0600

    little changes

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
 let canvas;
 let canvasContext;
+let snake = 50;
 
 
 window.onload = function () {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-
+    setInterval(drawSnake, 1000);
     drawEverything();
-    drawRect();
+
+    // drawApple();
 }
 
 function drawEverything() {
 
-    //Make a teal colored rectangle
-    canvasContext.fillStyle = 'rgba(0,255,255, 1.0)'
-    canvasContext.fillRect(0, 0, 100, 100)
-
-    //Make a pink rectangle
-    canvasContext.fillStyle = 'rgba(255,105,180, 1.0)'
-    canvasContext.fillRect(100, 100, 100, 100);
-}
-// figure out why loop is only going to one color
-function drawRect(x, y, width, height, color) {
-    canvasContext.fillStyle = color;
-    canvasContext.fillRect(x, y, width, height)
-
+    // Make teal rectangles at 200 px intervals
     for (let i = 0; i < canvas.width; i += 200) {
         for (let j = 0; j < canvas.height; j += 200) {
-            canvasContext.fillRect(i, j, 100, 100, 'rgba(0,255,255,1.0');
+            drawRect(i, j, 100, 100, "rgba(0,255,255, 1.0)");
         }
     }
 
+    // Make pink rectangles at 200 px intervals
     for (let i = 100; i < canvas.width; i += 200) {
         for (let j = 100; j < canvas.height; j += 200) {
-            canvasContext.fillRect(i, j, 100, 100, 'rgba(255,105,180,1.0)');
+            drawRect(i, j, 100, 100, "rgba(255,105,180, 1.0)");
         }
     }
 }
 
+function drawRect(x, y, width, height, color) {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, height);
+}
 
+function drawSnake() {
+    snake = snake + 10;
+    console.log(snake);
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(snake, 200, 50, 25);
+}
 
+// function drawApple() {
+//     canvasContext.fillStyle = 'red';
+//     canvasContext.fillRect(300, 200, 10, 15);
+// }
 
-
-// canvasContext.strokeStyle = "black";
-    // canvasContext.fillStyle = "blue";
-    // canvasContext.strokeRect(0, 0, 50, 50);
-    // canvasContext.fillRect(55, 0, 50, 50)
-    // canvasContext.fillStyle = pat;
-    // canvasContext.fill();
 
 
