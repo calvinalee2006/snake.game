ccd4ecff0e9ec9cc62c8a33c1b577c24a44d740f
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-02-22 18:22:37 -0600

    making progress?

--- script.js
+++ script.js
@@ -1,50 +1,74 @@
+//Drawing of the canvas
 let canvas;
 let canvasContext;
-let snake = 50;
+
+//Drawing of the snake 
+let tileCount = 20;
+let tileSize = 18;
+let headX = 10;
+let headY = 10;
+
+//control of the snake
+var rightPressed = false;
+var leftPressed = false;
 
 
 window.onload = function () {
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    setInterval(drawSnake, 1000);
-    drawEverything();
 
-    // drawApple();
+    let framesPerSecond = 30;
+    setInterval(function () {
+        drawGameBoard();
+        drawSnake();
+        drawApple();
+    }, 1000 / framesPerSecond);
 }
 
-function drawEverything() {
-
-    // Make teal rectangles at 200 px intervals
-    for (let i = 0; i < canvas.width; i += 200) {
-        for (let j = 0; j < canvas.height; j += 200) {
-            drawRect(i, j, 100, 100, "rgba(0,255,255, 1.0)");
-        }
-    }
-
-    // Make pink rectangles at 200 px intervals
-    for (let i = 100; i < canvas.width; i += 200) {
-        for (let j = 100; j < canvas.height; j += 200) {
-            drawRect(i, j, 100, 100, "rgba(255,105,180, 1.0)");
-        }
-    }
-}
-
-function drawRect(x, y, width, height, color) {
-    canvasContext.fillStyle = color;
-    canvasContext.fillRect(x, y, width, height);
+function drawGameBoard() {
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
 }
 
 function drawSnake() {
-    snake = snake + 10;
-    console.log(snake);
     canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(snake, 200, 50, 25);
+    canvasContext.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
 
-// function drawApple() {
-//     canvasContext.fillStyle = 'red';
-//     canvasContext.fillRect(300, 200, 10, 15);
+function drawApple() {
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(600, 250, 20, 20);
+}
+
+
+
+
+// drawRect(0, 0, canvas.width, canvas.height, "white")
+
+// function drawRect(x, y, width, height, color) {
+//     canvasContext.fillStyle = color;
+//     canvasContext.fillRect(x, y, width, height);
 // }
+
+
+
+
+
+
+
+ // Make teal rectangles at 200 px intervals
+    // for (let i = 0; i < canvas.width; i += 200) {
+    //     for (let j = 0; j < canvas.height; j += 200) {
+    //         drawRect(i, j, 100, 100, "rgba(0,255,255, 1.0)");
+    //     }
+    // }
+
+    // // Make pink rectangles at 200 px intervals
+    // for (let i = 100; i < canvas.width; i += 200) {
+    //     for (let j = 100; j < canvas.height; j += 200) {
+    //         drawRect(i, j, 100, 100, "rgba(255,105,180, 1.0)");
+    //     }
+    // }
 
 
 
