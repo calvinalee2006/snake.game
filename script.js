f19be26f777fd314a111e7c14d4d1f5acc7a624d
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-06-03 05:21:28 -0500

    finished

--- script.js
+++ script.js
@@ -1,230 +1,152 @@
-//Drawing of canvas
-let canvas = document.getElementById('gameCanvas');
-let canvasContext = canvas.getContext('2d');
+const SIZE = 20;
+const FPS = 30;
+const canvas = document.getElementById('gameCanvas');
+const canvasContext = canvas.getContext('2d');
 
 let score = 0;
 
-//Drawing of snake head
-let tileCount = 20;
-let tileSize = 18;
+let snake = {
+    body: [
+        { x: 200, y: 240 },
+        { x: 180, y: 240 },
+        { x: 160, y: 240 }
+    ],
+    direction: undefined,
+    size: 18,
+};
 
-//Drawing of the snake body
-let snake = [
-    { x: 200, y: 240 },
-    { x: 180, y: 240 },
-    { x: 160, y: 240 }
-];
-
-let snakeLength = [];
-
-let snakeDirection = null;
-
-let hitBody = false;
-
-let gameRestart = false;
-
-let myInterval = setInterval(gameOver);
-
-// Apple
 let apple = {
-    x: Math.floor((Math.random() * (canvas.width - 20)) / 20) * 20, y: Math.floor((Math.random() * (canvas.height - 20)) / 20) * 20
+    x: Math.floor((Math.random() * (canvas.width - 20)) / 20) * 20,
+    y: Math.floor((Math.random() * (canvas.height - 20)) / 20) * 20
 }
 
-window.onload = function () {
-    let framesPerSecond = 30;
-    setInterval(function () {
-        drawGameBoard();
-        drawSnake();
-        drawApple();
-        snakeScore()
-        snakeEatsApple();
-        moveSnake();
-        hitSnake();
-        wallCheck()
-        stopGameFunction()
-    }, 2500 / framesPerSecond);
+window.onload = () => setInterval(init, 2500 / FPS);
+
+function init() {
+    drawCanvas();
+    drawSnake();
+    drawApple();
+    drawScore()
+    moveSnake();
+    snakeEatsApple();
+    bodyCheck();
+    wallCheck()
 }
 
-function drawGameBoard() {
-    canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+document.addEventListener("keydown", setDirection);
+
+function setDirection(event) {
+    switch (event.key) {
+        case "ArrowUp":
+            if (snake.direction !== "down") snake.direction = "up";
+            break;
+        case "ArrowDown":
+            if (snake.direction !== "up") snake.direction = "down";
+            break;
+        case "ArrowRight":
+            if (snake.direction !== "left") snake.direction = "right";
+            break;
+        case "ArrowLeft":
+            if (snake.direction !== "right") snake.direction = "left";
+            break;
+        default:
+            break;
+    }
 }
 
-function hitSnake() {
+function drawRect(x, y, width, height, color) {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, height, width)
+}
+
+function drawCanvas() {
+    drawRect(0, 0, canvas.width, canvas.height, "white");
+}
+
+function drawSnake() {
+    snake.body.forEach((part) => {
+        drawRect(part.x, part.y, SIZE, SIZE, "green")
+    });
+}
+
+function drawApple() {
+    drawRect(apple.x, apple.y, SIZE, SIZE, "red")
+}
+
+function drawScore() {
+    canvasContext.font = "18px Arial";
+    canvasContext.fillStyle = "#0095DD";
+    canvasContext.fillText("Score: " + score, 8, SIZE);
+}
+
+function bodyCheck() {
     for (let i = 1; i < snake.length; i++) {
-        let hitBody = snake[i].x == snake[0].x && snake[i].y == snake[0].y;
-        if (hitBody === true) {
-            alert("Game Over, stop hitting yourself!")
-            gameOver()
+        if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+            alert("Game Over, stop hitting yourself!");
+            gameOver();
         }
     }
 }
 
 function wallCheck() {
-    const hitLeftWall = snake[0].x < 0;
-    const hitRightWall = snake[0].x > canvas.width + 10;
-    const hitTopWall = snake[0].y < 0;
-    const hitBottomWall = snake[0].y > canvas.height + 10;
-    if (hitLeftWall == true) {
-        alert('Game Over, You hit the left wall!')
-        gameOver()
-    }
-    if (hitRightWall == true) {
-        alert('Game Over, You hit the right wall!!');
+    const hitLeftWall = snake.body[0].x < 0 - SIZE;
+    const hitRightWall = snake.body[0].x > canvas.width;
+    const hitTopWall = snake.body[0].y < 0 - SIZE;
+    const hitBottomWall = snake.body[0].y > canvas.height;
+    if (hitLeftWall) alert('Game Over, You hit the left wall!')
+    if (hitRightWall) alert('Game Over, You hit the right wall!!');
+    if (hitTopWall) alert('Game Over, You hit the top wall!!');
+    if (hitBottomWall) alert('Game Over, You hit the bottom wall!!');
+
+    if (hitLeftWall || hitBottomWall || hitRightWall || hitTopWall) {
         gameOver();
-    }
-    if (hitTopWall == true) {
-        alert('Game Over, You hit the top wall!!')
-        gameOver()
-    }
-    if (hitBottomWall == true) {
-        alert('Game Over, You hit the bottom wall!!')
-        gameOver()
-    }
-}
-
-function drawSnake() {
-    snake.forEach((snakePart) => {
-        canvasContext.fillStyle = "green";
-        canvasContext.fillRect(snakePart.x, snakePart.y, tileSize, tileSize);
-    });
-}
-
-document.addEventListener("keydown", setSnakeDirection);
-
-function setSnakeDirection(event) {
-    //up
-    if (event.key === 'ArrowUp') {
-        if (snakeDirection !== "down") {
-            snakeDirection = "up";
-        }
-    }
-    //down
-    if (event.key === 'ArrowDown') {
-        if (snakeDirection !== "up") {
-            snakeDirection = "down";
-        }
-    }
-    // left
-    if (event.key === "ArrowLeft") {
-        if (snakeDirection !== "right") {
-            snakeDirection = "left";
-        }
-    }
-    // right
-    if (event.key === "ArrowRight") {
-        if (snakeDirection !== "left") {
-            snakeDirection = "right";
-        }
     }
 }
 
 function moveSnake() {
-    if (!snakeDirection) return;
-
-    //play with code!! 
-    for (let i = snake.length - 1; i > 0; i--) {
-        const previousPartIndex = i - 1;
-        const previousPart = snake[previousPartIndex]
-        snake[i] = Object.assign({}, previousPart)
+    if (!snake.direction) return;
+    for (let i = snake.body.length - 1; i > 0; i--) {
+        snake.body[i] = Object.assign({}, snake.body[i - 1])
     }
-
-    if (snakeDirection === "up") {
-        snake[0].y -= tileCount
-    }
-
-    if (snakeDirection === "down") {
-        snake[0].y += tileCount
-    }
-
-    if (snakeDirection === "right") {
-        snake[0].x += tileCount
-    }
-
-    if (snakeDirection === "left") {
-        snake[0].x -= tileCount
-    }
-}
-
-function drawApple() {
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(apple.x, apple.y, 20, 20);
+    if (snake.direction === "up") snake.body[0].y -= SIZE;
+    if (snake.direction === "down") snake.body[0].y += SIZE;
+    if (snake.direction === "right") snake.body[0].x += SIZE;
+    if (snake.direction === "left") snake.body[0].x -= SIZE;
 }
 
 function snakeEatsApple() {
-    if (snake[0].x === apple.x && snake[0].y === apple.y) {
-        apple = {
-            x: Math.floor((Math.random() * (canvas.width - 20)) / 20) * 20,
-            y: Math.floor((Math.random() * (canvas.height - 20)) / 20) * 20,
-        }
+    if (snake.body[0].x === apple.x && snake.body[0].y === apple.y) {
+        generateRandomApplePosition();
+        growSnake();
         score++;
-
-        let newHead = {
-            x: snake[0].x,
-            y: snake[0].y
-        }
-        snake.push(newHead);
     }
-};
+}
 
-function snakeScore() {
-    canvasContext.font = "18px Arial";
-    canvasContext.fillStyle = "#0095DD";
-    canvasContext.fillText("Score: " + score, 8, 20);
+function generateRandomApplePosition() {
+    apple = {
+        x: Math.floor((Math.random() * (canvas.width - 20)) / 20) * 20,
+        y: Math.floor((Math.random() * (canvas.height - 20)) / 20) * 20,
+    };
+}
+
+function growSnake() {
+    let newHead = {
+        x: snake.body.x,
+        y: snake.body.y
+    };
+    snake.body.push(newHead);
 }
 
 function gameOver() {
-    score = 0;
-    snake = [{ x: 200, y: 240 },
-    { x: 180, y: 240 },
-    { x: 160, y: 240 }
-    ];
+    score.body = 0;
+    snake = {
+        body: [
+            { x: 200, y: 240 },
+            { x: 180, y: 240 },
+            { x: 160, y: 240 }
+        ],
+        direction: undefined,
+        size: 18,
+    };
+    generateRandomApplePosition();
 }
-
-function stopGameFunction() {
-    clearInterval(myInterval);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-//boarder
-
-
-// drawRect(0, 0, canvas.width, canvas.height, "white")
-
-// function drawRect(x, y, width, height, color) {
-//     canvasContext.fillStyle = color;
-//     canvasContext.fillRect(x, y, width, height);
-// }
-
-
-
-
-
-
-
- // Make teal rectangles at 200 px intervals
-    // for (let i = 0; i < canvas.width; i += 200) {
-    //     for (let j = 0; j < canvas.height; j += 200) {
-    //         drawRect(i, j, 100, 100, "rgba(0,255,255, 1.0)");
-    //     }
-    // }
-
-    // // Make pink rectangles at 200 px intervals
-    // for (let i = 100; i < canvas.width; i += 200) {
-    //     for (let j = 100; j < canvas.height; j += 200) {
-    //         drawRect(i, j, 100, 100, "rgba(255,105,180, 1.0)");
-    //     }
-    // }
-
-
-
